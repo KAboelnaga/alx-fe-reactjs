b996f47951dfcf64c7ca8931f67a9a712f6c6af6
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-08-22 06:03:00 +0300

    Add HomePage component and recipe data

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,25 @@
+import {useState, useEffect} from "react";
+import data from '../data.json';
+
+function HomePage() {
+    const [recipes, setRecipes] = useState([])
+    useEffect(( ) => {
+        setRecipes(data);
+    }, []
+    )
+    return (
+        <div>
+        <h2>Recipes List</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {recipes.map((recipe) => (
+                <div key={recipe.id} className="border p-4 rounded shadow">
+                    <img src={recipe.image} alt={recipe.title} className="w-full"/>
+                    <h2 className="mt-2">{recipe.title}</h2>
+                    <p className="mt-2">{recipe.summary}</p>
+                </div>
+            ))}
+        </div>
+        </div>
+    )
+}
+export default HomePage;
